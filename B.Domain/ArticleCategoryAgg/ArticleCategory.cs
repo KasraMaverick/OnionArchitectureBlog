2aef5c538986f02fcf6f233b8cdc694b54e94d38
nadri.k <nadri.k@daneshrefah.ir>
2024-11-03 08:34:05 +0330

    Add project files.

---
+++ B.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace B.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory
+    {
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+        public ArticleCategory(string title)
+        {
+            Title = title;
+            IsDeleted = false;
+            CreatedDate = DateTime.Now;
+        }
+    }
+}
