86106ae3ee132edb0285fbccee50b5d398db6f33
nadri.k <nadri.k@daneshrefah.ir>
2024-11-03 13:34:48 +0330

    Added a UI Layer

---
+++ B.Presentation.Blazor/Areas/Admin/Pages/ArticleCategoryManagement/ArticleCategory.razor.cs
@@ -0,0 +1,25 @@
+﻿using B.Application.Contracts.ArticleCategory;
+
+namespace B.Presentation.Blazor.Areas.Admin.Pages.ArticleCategoryManagement
+{
+    public partial class ArticleCategory
+    {
+        public List<ArticleCategoryViewModel> ArticleCategories { get; set; } = new();
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+        public ArticleCategory(IArticleCategoryApplication articleCategoryApplication)
+        {
+            _articleCategoryApplication = articleCategoryApplication;   
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetAllArticleCategories();
+        }
+
+        public async Task GetAllArticleCategories()
+        {
+            var result = await _articleCategoryApplication.GetAllArticleCategories();
+            ArticleCategories = result;
+        }
+    }
+}
---
+++ B.Presentation.Blazor/Program.cs
@@ -0,0 +1,13 @@
+using B.Presentation.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+builder.Services.AddRadzenComponents();
