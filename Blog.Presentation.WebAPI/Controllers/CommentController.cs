ad3869396b5967ee59f8da8b0a31a3661091548f
Dr.Tazari <Dr.Tazari@TABLET-RMK1229U>
2024-11-17 15:55:07 +0330

    did the WebApi section of the Comment template

---
+++ Blog.Presentation.WebAPI/Controllers/CommentController.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Provider.Contracts.Comment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Presentation.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRequestProvider _commentRequestProvider;
+        public CommentController(ICommentRequestProvider commentRequestProvider)
+        {
+            _commentRequestProvider = commentRequestProvider;
+        }
+    }
+}
