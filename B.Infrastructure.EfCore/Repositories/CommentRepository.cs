60be9b6d1c629985748b47da5bd72689a9d2d3a8
Dr.Tazari <Dr.Tazari@TABLET-RMK1229U>
2024-11-17 15:38:24 +0330

    Did the Infrastructure.EfCore section of the Comment template

---
+++ B.Infrastructure.EfCore/Repositories/CommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Management.Domain.CommentAgg;
+using Blog.Management.Infrastructure.EfCore.Repositories.Shared;
+
+namespace Blog.Management.Infrastructure.EfCore.Repositories
+{
+    public class CommentRepository : Repository<Comment>, ICommentRepository
+    {
+        private readonly BlogContext _dbContext;
+        public CommentRepository(BlogContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
