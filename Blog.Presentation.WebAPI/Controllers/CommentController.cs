3370f6120f295c91a1a377d7b2bc1a3de46898d8
KasraNadri <kasra.n2018@gmail.com>
2024-11-18 16:27:31 +0330

    Completed the WebAPI section of the Comment template

--- Blog.Presentation.WebAPI/Controllers/CommentController.cs
+++ Blog.Presentation.WebAPI/Controllers/CommentController.cs
@@ -1,4 +1,6 @@
-﻿using Blog.Provider.Contracts.Comment;
+﻿using _0_Framework.Application.Model;
+using Blog.Management.Application.Contracts.Comment.Dtos;
+using Blog.Provider.Contracts.Comment;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Presentation.WebAPI.Controllers
@@ -15,5 +17,33 @@ namespace Blog.Presentation.WebAPI.Controllers
         }
 
         //--------------------------------------------------------------------------------------------------------------
+
+        [HttpGet("GetAll")]
+        public async Task<OperationResultWithData<List<GetCommentForArticleDto>>> GetAll()
+        {
+            var list = await _commentRequestProvider.GetAll();
+            return list;
+        }
+
+        [HttpPost("Create")]
+        public async Task<OperationResult> Create(CreateCommentDto dto)
+        {
+            var res = await _commentRequestProvider.Create(dto);
+            return res;
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<OperationResult> Delete(DeleteCommentDto dto)
+        {
+            var res = await _commentRequestProvider.Delete(dto);
+            return res;
+        }
+
+        [HttpPut("Update")]
+        public async Task<OperationResult> Edit(EditCommentDto dto)
+        {
+            var res = await _commentRequestProvider.Update(dto);
+            return res;
+        }
     }
 }
