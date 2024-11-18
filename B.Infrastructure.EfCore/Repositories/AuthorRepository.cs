87729b0e1e27340bb89b79699c0268e158889335
KasraNadri <kasra.n2018@gmail.com>
2024-11-18 15:54:50 +0330

    Did the Infrastructure.EfCore of the Author Template

---
+++ B.Infrastructure.EfCore/Repositories/AuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Management.Domain.AuthorAgg;
+using Blog.Management.Infrastructure.EfCore.Repositories.Shared;
+
+namespace Blog.Management.Infrastructure.EfCore.Repositories
+{
+    public class AuthorRepository : Repository<Author>, IAuthorRepository
+    {
+        private readonly BlogContext _dbContext;
+        public AuthorRepository(BlogContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext; 
+        }
+    }
+}
