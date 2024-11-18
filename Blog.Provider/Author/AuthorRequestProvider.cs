79442d46d6eb0eceef4433a1d5138e13a8cb289d
KasraNadri <kasra.n2018@gmail.com>
2024-11-18 16:06:55 +0330

    Did the Provider section of the Author template

---
+++ Blog.Provider/Author/AuthorRequestProvider.cs
@@ -0,0 +1,36 @@
+﻿using _0_Framework.Application.Model;
+using Blog.Management.Application.Contracts.Author;
+using Blog.Management.Application.Contracts.Author.Dtos;
+using Blog.Provider.Contracts.Author;
+
+namespace Blog.Provider.Author
+{
+    public class AuthorRequestProvider : IAuthorRequestProvider
+    {
+        private readonly IAuthorApplication _authorApplication;
+        public AuthorRequestProvider(IAuthorApplication authorApplication)
+        {
+            _authorApplication = authorApplication;
+        }
+
+        public Task<OperationResult> Create(CreateAuthorDto author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Delete(DeleteAuthorDto author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResultWithData<List<GetAuthorDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult> Update(UpdateAuthorDto author)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
