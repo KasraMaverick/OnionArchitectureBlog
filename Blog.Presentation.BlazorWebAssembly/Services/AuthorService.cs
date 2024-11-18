5fc2ab693de03c0155de8d3d0c35760c0a9594af
KasraNadri <kasra.n2018@gmail.com>
2024-11-18 16:32:38 +0330

    Did the BlazorWebAssembly section of the Author template

---
+++ Blog.Presentation.BlazorWebAssembly/Services/AuthorService.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.Presentation.BlazorWebAssembly.Services
+{
+    public class AuthorService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public AuthorService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7170/api/");
+        }
+    }
+}
