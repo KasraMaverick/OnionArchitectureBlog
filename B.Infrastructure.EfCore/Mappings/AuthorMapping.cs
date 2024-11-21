47a4ef01f4f6a838c95ebb7d1ffa8c10cbc06a1e
Dr.Tazari <Dr.Tazari@TABLET-RMK1229U>
2024-11-21 16:13:21 +0330

    Registered the mapping of Author and Comment

--- B.Infrastructure.EfCore/Mappings/AuthorMapping.cs
+++ B.Infrastructure.EfCore/Mappings/AuthorMapping.cs
@@ -9,6 +9,16 @@ namespace Blog.Management.Infrastructure.EfCore.Mappings
     {
         public void Configure(EntityTypeBuilder<Author> builder)
         {
+            builder.HasKey(x => x.AuthorId);
+            builder.Property(x => x.FirstName);
+            builder.Property(x => x.LastName);
+            builder.Property(x => x.UserId);
+            builder.Property(x => x.ImageUrl);
+            builder.Property(x => x.Bio);
+            builder.Property(x => x.ArticleCount);
+            builder.Property(x => x.CreatedDate);
+            builder.Property(x => x.IsActive);
+
 
             builder.HasMany(x => x.Articles).WithOne(x => x.Author).HasForeignKey(x => x.AuthorId);
             builder.HasMany(x => x.Comments).WithOne(x => x.Author).HasForeignKey(x => x.AuthorId);
--- B.Infrastructure.EfCore/Mappings/CommentMapping.cs
+++ B.Infrastructure.EfCore/Mappings/CommentMapping.cs
@@ -8,7 +8,16 @@ namespace Blog.Management.Infrastructure.EfCore.Mappings
     {
         public void Configure(EntityTypeBuilder<Comment> builder)
         {
-            
+            builder.HasKey(x => x.CommentId);
+            builder.Property(x => x.CommentText);
+            builder.Property(x => x.CreatedDate);
+            builder.Property(x => x.LastUpdatedDate);
+            builder.Property(x => x.IsDeleted);
+            builder.Property(x => x.LikesCount);
+            builder.Property(x => x.DislikesCount);
+
+
+
             builder.HasOne(x => x.Author).WithMany(x => x.Comments).HasForeignKey(x => x.AuthorId);
             builder.HasOne(x => x.Article).WithMany(x => x.Comments).HasForeignKey(x => x.ArticleId);
         }
