b515b2c9fdb52fb85b26fdb209a7a3032611c26e
KasraNadri <kasra.n2018@gmail.com>
2024-11-23 15:57:56 +0330

    completed article dtos

--- B.Application.Contracts/Article/Dtos/CreateArticleDto.cs
+++ B.Application.Contracts/Article/Dtos/CreateArticleDto.cs
@@ -2,5 +2,10 @@
 {
     public class CreateArticleDto
     {
+        public long CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Excerpt { get; set; }
+        public string FeaturedImage { get; set; }
     }
 }
--- B.Application.Contracts/Article/Dtos/DeleteArticleDto.cs
+++ B.Application.Contracts/Article/Dtos/DeleteArticleDto.cs
@@ -2,5 +2,6 @@
 {
     public class DeleteArticleDto
     {
+        public long ArticleId { get; set; }
     }
 }
