9046abf569d21ab5e4689608c7d564aa58ec10d4
KasraNadri <kasra.n2018@gmail.com>
2024-12-02 07:39:37 +0330

    added logservice and removed it from ignore

---
+++ 0_Framework/Log/ILogService.cs
@@ -0,0 +1,11 @@
+﻿namespace _0_Framework.Log
+{
+    public interface ILogService
+    {
+        void LogInformation(string location, string message);
+        void LogError(string location, string message);
+        void LogWarning(string location, string message);
+        //---------
+
+    }
+}
---
+++ 0_Framework/Log/LogService.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Serilog;
+
+namespace _0_Framework.Log
+{
+    public class LogService : ILogService
+    {
+        private readonly ILogger _seriLog;
+        public LogService(ILogger seriLog)
+        {
+            _seriLog = seriLog;
+        }
+        public void LogError(string location, string message)
+        {
+            _seriLog.Error(@$"Location: {location} ||| Message: {message}");
+        }
+
+        public void LogInformation(string location, string message)
+        {
+            _seriLog.Information(message);
+        }
+
+        public void LogWarning(string location, string message)
+        {
+            _seriLog.Warning(message);
+        }
+
+        public void LogException(Exception ex, string className, string message)
+        {
+            var stackTrace = new StackTrace(ex, true); // `true` includes file information
+            var location = stackTrace?.GetFrame(0)?.GetFileName();  // File name
+            var lineNumber = stackTrace?.GetFrame(0)?.GetFileLineNumber();  // Line number
+
+            _seriLog.Error(ex, message, className, location, lineNumber);
+        }
+    }
+}
