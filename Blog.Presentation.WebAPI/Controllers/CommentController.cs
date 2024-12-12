145daa666a9a8dc73e616847eb110294bfe944d2
Kasra Nadri <kasra.n2018@gmail.com>
2024-12-12 10:36:44 +0330

    implemented commentcontroller

--- Blog.Presentation.WebAPI/Controllers/CommentController.cs
+++ Blog.Presentation.WebAPI/Controllers/CommentController.cs
@@ -9,19 +9,24 @@ namespace Blog.Presentation.WebAPI.Controllers
     [ApiController]
     public class CommentController : ControllerBase
     {
-        //-------------------- INJECTIONS --------------------\\
+
+        #region INJECTION
+
         private readonly ICommentRequestProvider _commentRequestProvider;
         public CommentController(ICommentRequestProvider commentRequestProvider)
         {
             _commentRequestProvider = commentRequestProvider;
         }
 
-        //--------------------------------------------------------------------------------------------------------------
+        #endregion
+
+
+        #region CRUD
 
         [HttpGet("GetAll")]
-        public async Task<OperationResultWithData<List<GetCommentForArticleDto>>> GetAll()
+        public async Task<OperationResultWithData<List<GetCommentForArticleDto>>> GetAll(long articleId)
         {
-            var list = await _commentRequestProvider.GetAll();
+            var list = await _commentRequestProvider.GetAll(articleId);
             return list;
         }
 
@@ -32,18 +37,66 @@ namespace Blog.Presentation.WebAPI.Controllers
             return res;
         }
 
-        [HttpDelete("Delete")]
-        public async Task<OperationResult> Delete(DeleteCommentDto dto)
-        {
-            var res = await _commentRequestProvider.Delete(dto);
-            return res;
-        }
-
         [HttpPut("Update")]
         public async Task<OperationResult> Edit(EditCommentDto dto)
         {
             var res = await _commentRequestProvider.Update(dto);
             return res;
         }
+
+        #endregion
+
+
+        #region LIKE/DISLIKE
+
+        [HttpPut("Like")]
+        public async Task<OperationResult> Like(long commentId)
+        {
+            var res = await _commentRequestProvider.LikeComment(commentId);
+            return res;
+        }
+
+        [HttpPut("Dislike")]
+        public async Task<OperationResult> Dislike(long commentId)
+        {
+            var res = await _commentRequestProvider.DislikeComment(commentId);
+            return res;
+        }
+
+        #endregion
+
+
+        #region ACTIVATE/DEACTIVATE FOR ARTICLE & AUTHOR
+
+        [HttpPut("ActivateForArticle")]
+        public async Task<OperationResult> ActivateForArticle(long articleId)
+        {
+            var res = await _commentRequestProvider.ActivateForArticle(articleId);
+            return res;
+        }
+
+        [HttpPut("DeactivateForArticle")]
+        public async Task<OperationResult> DeactivateForArticle(long articleId)
+        {
+            var res = await _commentRequestProvider.DeactivateForArticle(articleId);
+            return res;
+        }
+
+        [HttpPut("ActivateForAuthor")]
+        public async Task<OperationResult> ActivateForAuthor(long authorId)
+        {
+            var res = await _commentRequestProvider.ActivateForAuthor(authorId);
+            return res;
+        }
+
+        [HttpPut("DeactivateForAuthor")]
+        public async Task<OperationResult> DeactivateForAuthor(long authorId)
+        {
+            var res = await _commentRequestProvider.DeactivateForAuthor(authorId);
+            return res;
+        }
+
+        #endregion
+
     }
 }
